a5f7efb7e916b9eaded57b0f602eb8ec90d33dcc
Gustavo Santos <gustavo.santos1601@outlook.com>
2022-11-02 01:27:01 -0300

    new files

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <header class="header">
+      <strong>
+        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
+      </strong>
+      <nav class="nav">
+        <g-link class="" to="/">Home</g-link>
+        <g-link class="" to="/portfolio/">About</g-link>
+        <g-link class="" to="/contato/">About</g-link>
+      </nav>
+    </header>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Contato'
+  }
+}
+</script>
